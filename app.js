39d6568f45de4fab67acd96271cd29bb4b80916f
Muhammad Bilal Siddiqui <muhammad.bilal.siddiqui.11@gmail.com>
2020-07-11 03:27:08 +0500

    v2.5 (Bug fixes, enhanced UI)

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var saveCounter = 0; /* to count save items */
 var min = 0;
 var sec = 0;
 var msec = 0;
+var hour = 0;
 /* reading the fields */
 var minCounter = document.getElementById("min");
 var secCounter = document.getElementById("sec");
 var msecCounter = document.getElementById("msec");
+var hourCounter = document.getElementById("hour")
 var startBtnId = document.getElementById("startBtn");
 var pauseBtnId = document.getElementById("pauseBtn");
 var historyId = document.getElementById("history");
@@ -23,10 +25,15 @@ function stopWatch() {
         sec++;
         secCounter.innerHTML = sec;
     }
-    else if (sec === 60) {
+    else if (sec === 30) {
         sec = 0;
         min++;
         minCounter.innerHTML = min;
+    }
+    else if (min === 1) {
+        min = 0;
+        hour++;
+        hourCounter.innerHTML = hour;
     }
 }
 
@@ -49,9 +56,11 @@ function reset() {
     msec = 0;
     sec = 0;
     min = 0;
+    hour = 0;
     msecCounter.innerHTML = "00";
     secCounter.innerHTML = "00";
     minCounter.innerHTML = "00";
+    hourCounter.innerHTML = "00";
 
     clearInterval(interval);
     startBtnId.disabled = false; /* enabling the start button */
@@ -61,22 +70,23 @@ function reset() {
 }
 
 /* save function */
-function save() {
+function lap() {
     startBtnId.disabled = false; /* enabling the start button */
 
+    var currentHour = hourCounter.innerHTML;
     var currentMin = minCounter.innerHTML;
     var currentSec = secCounter.innerHTML;
     var currentMsec = msecCounter.innerHTML;
 
     /* saving the currently displayed time in log  */
     saveCounter++;
-    historyId.innerHTML = saveCounter + ". &nbsp;&nbsp;&nbsp;&nbsp;" + currentMin + " : " + currentSec + " : " + currentMsec;
+    historyId.innerHTML += "<br>" + saveCounter + ". &nbsp;&nbsp;&nbsp;&nbsp;" + currentHour + " : " + currentMin + " : " + currentSec;
     /* if we want to limit user to save only 15 entreies in log */
-/*     if (saveCounter === 16) {
-        alert("The 'History Log' limit exceeded. If you click on 'Save' again, all current log data will be lost.");
-    }
-    if (saveCounter === 17) {
-        saveCounter = 0;
-        historyId.innerHTML = ""
-    } */
+    /*     if (saveCounter === 16) {
+            alert("The 'History Log' limit exceeded. If you click on 'Save' again, all current log data will be lost.");
+        }
+        if (saveCounter === 17) {
+            saveCounter = 0;
+            historyId.innerHTML = ""
+        } */
 }
